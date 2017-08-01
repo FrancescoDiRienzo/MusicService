077ef82f811ee0a5066ed02a6fa568c8c80d3de3
FrancescoDiRienzo <Francesco_fdr@libero.it>
2017-08-01 12:43:53 +0200

    Initial commit

---
+++ app/src/main/java/util/Song.java
@@ -0,0 +1,21 @@
+package util;
+
+/**
+ * Created by Franc on 01/08/2017.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
